5061e5185224d82414f6ea1a5b6545cd97ea3c9f
david cuspoca <david-cuspoca@hotmail.com>
2023-07-23 00:12:46 -0600

    ruta.module.ts se agregaron algunos modelos necesarios
    para poder hacer operaciones con la rutruta.module.ts se agregaron
    algunos modelos necesarios
    
    para poder hacer operaciones con la rutaa

--- src/ruta/ruta.module.ts
+++ src/ruta/ruta.module.ts
@@ -5,6 +5,12 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Ruta, RutaSchema } from './entities/ruta.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { Credito, CreditoSchema } from '../credito/entities/credito.entity';
+import { Gasto, GastoSchema } from '../gasto/entities/gasto.entity';
+import { Cliente, ClienteSchema } from '../cliente/entities/cliente.entity';
+import { Inversion, InversionSchema } from '../inversion/entities/inversion.entity';
+import { Retiro, RetiroSchema } from '../retiro/entities/retiro.entity';
+import { Caja, CajaSchema } from '../caja/entities/caja.entity';
 
 @Module({
   imports: [
@@ -13,6 +19,30 @@ import { AuthModule } from 'src/auth/auth.module';
       {
         name: Ruta.name,
         schema: RutaSchema
+      },
+      {
+        name: Credito.name,
+        schema: CreditoSchema
+      },
+      {
+        name: Gasto.name,
+        schema: GastoSchema
+      },
+      {
+        name: Cliente.name,
+        schema: ClienteSchema
+      },
+      {
+        name: Inversion.name,
+        schema: InversionSchema
+      },
+      {
+        name: Retiro.name,
+        schema: RetiroSchema
+      },
+      {
+        name: Caja.name,
+        schema: CajaSchema
       }
     ]),
     AuthModule
