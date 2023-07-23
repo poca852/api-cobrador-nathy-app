4fd206d5ea23a473ed75ff41f270336891724b3c
david cuspoca <david-cuspoca@hotmail.com>
2023-07-23 00:13:34 -0600

    ruta.service.ts se agregaron la mayoria de servicios
    como eliminar listar, cerrar ruta y demas este un esta incompleto

--- src/ruta/ruta.service.ts
+++ src/ruta/ruta.service.ts
@@ -1,41 +1,87 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, Logger, BadRequestException, InternalServerErrorException } from '@nestjs/common';
 import { CreateRutaDto } from './dto/create-ruta.dto';
 import { UpdateRutaDto } from './dto/update-ruta.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Ruta } from './entities/ruta.entity';
 import { Model, isValidObjectId } from 'mongoose';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../auth/entities/user.entity';
+import { Credito } from '../credito/entities/credito.entity';
+import { Cliente } from '../cliente/entities/cliente.entity';
+import { Inversion } from '../inversion/entities/inversion.entity';
+import { Gasto } from '../gasto/entities/gasto.entity';
+import { Retiro } from '../retiro/entities/retiro.entity';
+import { GlobalParams } from '../common/dto/global-params.dto';
+import { Caja } from '../caja/entities/caja.entity';
 
 @Injectable()
 export class RutaService {
 
+  private logger = new Logger("RutaService");
+
   constructor(
     @InjectModel(Ruta.name)
-    private readonly rutaModel: Model<Ruta>
+    private readonly rutaModel: Model<Ruta>,
+
+    @InjectModel(Credito.name)
+    private readonly creditoModel: Model<Credito>,
+
+    @InjectModel(Cliente.name)
+    private readonly clienteModel: Model<Cliente>,
+
+    @InjectModel(Inversion.name)
+    private readonly inversionModel: Model<Inversion>,
+
+    @InjectModel(Gasto.name)
+    private readonly gastoModel: Model<Gasto>,
+
+    @InjectModel(Retiro.name)
+    private readonly retiroModel: Model<Retiro>,
+
+    @InjectModel(Caja.name)
+    private readonly cajaModel: Model<Caja>,
+
+    private readonly authService: AuthService
   ){}
 
-  create(createRutaDto: CreateRutaDto) {
-    return 'This action adds a new ruta';
-  }
-
-  async findAll(): Promise<Ruta[]> {
-    return await this.rutaModel.find();
-  }
-
-  async findOne(termino: string): Promise<Ruta> {
+  async create(createRutaDto: CreateRutaDto) {
     
-    let ruta: Ruta;
+    const { userId, ...data } = createRutaDto;
 
-    if(isValidObjectId(termino)){
-      ruta = await this.rutaModel.findById(termino);
+    const user = await this.authService.findOne(userId);
+
+    try {
+
+      const ruta = await this.rutaModel.create(data); 
+
+      user.rutas.push(ruta._id);
+      await user.save();
+
+      return ruta;
+
+    } catch (error) {
+
+      this.handleExceptions(error)
+
     }
+
+  }
+
+  async findAll(user: User): Promise<Ruta[]> {
+    
+    return (await this.authService.findOne(user._id)).rutas
+
+  }
+
+  async findOne(id: string): Promise<Ruta> {
+    
+    const ruta = await this.rutaModel.findById(id);
 
     if(!ruta){
-      ruta = await this.rutaModel.findOne({nombre: termino.trim().toUpperCase()})
+      throw new NotFoundException(`No existe una ruta con el id ${id}`);
     }
 
-    if(!ruta){
-      throw new NotFoundException(`No existe la ruta ${termino}`);
-    }
+    await this.actualizarRuta(ruta);
 
     return ruta;
 
@@ -51,6 +97,150 @@ export class RutaService {
       ...ruta.toJSON(),
       ...updateRutaDto
     }
+
+  }
+
+  async delete(id: string, globalParams: GlobalParams): Promise<boolean> {
+    const { userId } = globalParams;
+    const user = await this.authService.findOne(userId);
+
+    await this.rutaModel.findByIdAndDelete(id);
+
+    user.rutas = user.rutas.filter(ruta => ruta._id !== id);
+    await user.save();
+
+    return true;
+  }
+
+  async closeRuta(id: string, fecha: string) {
+
+    const ruta = await this.findOne(id);
+    const caja = await this.cajaModel.findOne({
+      ruta: ruta._id,
+      fecha: fecha.trim()
+    });
+
+    await ruta.updateOne({
+      status: false,
+      ultimo_cierre: fecha.trim(),
+      ultima_caja: caja._id,
+      turno: 1
+    }, {new: true});
+
+    await this.actualizarRuta(ruta);
+
+    return true;
+
+  }
+
+  async openRuta(id: string, fecha: string): Promise<boolean> {
+
+    const ruta = await this.findOne(id);
+    const ultimCaja = await this.cajaModel.findById(ruta.ultima_caja);
+
+    let caja;
+
+    if(!ultimCaja) {
+      caja = await this.cajaModel.create({
+        fecha: fecha.trim(),
+        ruta: id
+      })
+    }
+
+    if(ultimCaja){
+      // pendiente generar el actualizar caja
+      caja = await this.cajaModel.create({
+        base: ultimCaja.caja_final,
+        caja_final: ultimCaja.caja_final,
+        ruta: id,
+        fecha: fecha.trim()
+      })
+    }
+
+    await ruta.updateOne({
+      caja_actual: caja._id,
+      status: true,
+      ultima_apertura: fecha.trim()
+    })
+
+    return true;
+  }
+
+  private handleExceptions(error: any) {
+    if(error.code === 11000){
+      throw new BadRequestException("Ya existe esta ruta");
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException("Revisar los logs")
+  }
+
+  async actualizarRuta(ruta: Ruta): Promise<boolean> {
+
+    const rutaDB = await this.rutaModel.findById(ruta._id);
+
+    if(!rutaDB) {
+      throw new NotFoundException(`No existe una ruta con el id ${ruta._id}`);
+    }
+
+    let cartera: number = 0;
+    let total_cobrado: number = 0;
+    let gastos: number = 0;
+    let retiros: number = 0;
+    let inversiones: number = 0;
+    let total_prestado: number = 0;
+
+    let [ clientes, 
+          clientes_activos, 
+          creditosActivos, 
+          allCreditos,
+          allGastos,
+          allRetiros,
+          allInversiones ] = await Promise.all([
+      this.clienteModel.countDocuments({ruta: rutaDB._id}),
+      this.clienteModel.countDocuments({ruta: rutaDB._id, status: true}),
+      this.creditoModel.find({ruta: rutaDB._id, status: true}),
+      this.creditoModel.find({ruta: rutaDB._id}),
+      this.gastoModel.find({ruta: rutaDB._id}),
+      this.retiroModel.find({ruta: rutaDB._id}),
+      this.inversionModel.find({ruta: rutaDB._id})
+    ])
+
+    
+    creditosActivos.forEach(credito => {
+      cartera += credito.saldo;
+    });
+
+    allCreditos.forEach(credito => {
+      total_cobrado += credito.abonos;
+      total_prestado += credito.valor_credito;
+    })
+
+    allGastos.forEach(gasto => {
+      gastos += gasto.valor;
+    })
+    
+    allRetiros.forEach(retiro => {
+      retiros += retiro.valor;
+    })
+
+    allInversiones.forEach(inversion => {
+      inversiones += inversion.valor;
+    })
+
+
+    await ruta.updateOne({
+      cartera,
+      total_cobrado,
+      gastos,
+      retiros,
+      inversiones,
+      total_prestado,
+      clientes,
+      clientes_activos,
+    }, {new: true});
+
+    return true;
 
   }
 }
