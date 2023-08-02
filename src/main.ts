27207573f58ede000f0dc9f01853d8013217a12b
david cuspoca <david-cuspoca@hotmail.com>
2023-08-02 10:09:38 -0600

    main.ts actualizado

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,11 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
     })
   )
 
