cb442455907591f33631d97fea0127e430e6b520
david cuspoca <david-cuspoca@hotmail.com>
2023-08-30 22:31:16 -0600

    En la clase que me genera el informe del credito
    se implemento por separado el tema para poder generar los dias de
    atraso, cabe mencionar que esto esta por mejorarse. para poder elevar
    todo en una parte mas alta ya que de aca tambien depende en
    credito.serivce

--- src/credito/gnerar-informe-credito.ts
+++ src/credito/gnerar-informe-credito.ts
@@ -5,7 +5,7 @@ export class InformeCredito {
 
    private credito: Credito;
 
-   constructor( credito: Credito ){
+   constructor(credito: Credito) {
       this.credito = credito;
    }
 
@@ -16,52 +16,76 @@ export class InformeCredito {
    }
 
    public async calcularAtrasos() {
-      let q = this.credito.fecha_inicio.split('/')
-      let newFecha = `${q[2]}-${q[1]}-${q[0]}`;
-  
-      const fechaInicioCredito = new Date(newFecha);
-  
-      const fechaInicioPagos = addDays(fechaInicioCredito, 1);
-      
-      const fechaActual = new Date();
-    
-      const totalDias = differenceInDays(fechaActual, fechaInicioPagos);
-    
-      let diasEfectivosPago = totalDias;
-      let fecha = fechaInicioPagos;
-      for (let i = 0; i < totalDias; i++) {
-        if (getDay(fecha) === 0) {
-          diasEfectivosPago--;
-        }
-        fecha = addDays(fecha, 1);
-      }
-    
-      const valorCuota = this.credito.valor_cuota;
-      const totalAbonos = this.credito.abonos;
-    
-      const pagosRealizados = Math.floor(totalAbonos / valorCuota);
-      const pagosRequeridos = this.credito.total_cuotas;
-    
-      const atrasos = Math.max(diasEfectivosPago - pagosRealizados, 0);
-      const atrasosMaximos = (pagosRequeridos - 1) * 1; // Restamos 1 para representar el día en que se dio el crédito
-    
-      return Math.min(atrasos, atrasosMaximos);
-    }
 
-   async getMessage(): Promise<{message: string; urlMessage: string}> {
+      const credito = this.credito;
+
+      let q = credito.fecha_inicio.split('/');
+
+      let newFecha = `${q[2]}-${q[1]}-${q[0]}`;
+
+
+      const fechaInicioCredito = new Date(newFecha);
+      let fechaInicioPagos = addDays(fechaInicioCredito, 1);
+      const fechaActual = new Date();
+
+      const totalDias = differenceInDays(fechaActual, fechaInicioPagos);
+      const diasEfectivosPago = this.calcularDiasEfectivosPago(fechaInicioPagos, fechaActual, credito.se_cobran_domingos);
+
+      const intervaloPagos = this.calcularIntervaloPagos(credito.frecuencia_cobro);
+
+      const valorCuota = credito.valor_cuota;
+      const totalAbonos = credito.abonos;
+
+      const pagosRealizados = Math.floor(totalAbonos / valorCuota);
+      const pagosRequeridos = credito.total_cuotas;
+
+      const atrasos = Math.max(diasEfectivosPago - pagosRealizados, 0);
+      const atrasosMaximos = this.calcularAtrasosMaximos(pagosRequeridos, intervaloPagos);
+
+      return Math.min(atrasos, atrasosMaximos);
+   }
+
+   private calcularDiasEfectivosPago(fechaInicioPagos: Date, fechaActual: Date, seCobranDomingos: boolean) {
+      let diasEfectivosPago = differenceInDays(fechaActual, fechaInicioPagos);
+      if (!seCobranDomingos) {
+         let fecha = fechaInicioPagos;
+         for (let i = 0; i <= diasEfectivosPago; i++) {
+            if (getDay(fecha) === 0) {
+               diasEfectivosPago--;
+            }
+            fecha = addDays(fecha, 1);
+         }
+      }
+
+      return diasEfectivosPago;
+   }
+
+   private calcularIntervaloPagos(frecuenciaCobro: string): number {
+      if (frecuenciaCobro === 'semanal') {
+         return 7;
+      } else {
+         return 1;
+      }
+   }
+
+   private calcularAtrasosMaximos(pagosRequeridos: number, intervaloPagos: number): number {
+      return (pagosRequeridos - 1) * intervaloPagos;
+   }
+
+   async getMessage(): Promise<{ message: string; urlMessage: string }> {
 
       let cuotasPendientes = this.credito.saldo / this.credito.valor_cuota;
       let aplicandoFixed = cuotasPendientes.toFixed(2);
 
       let cuotasPendientesParse = Number(aplicandoFixed);
 
-      let txtEncoded: string =  `
+      let txtEncoded: string = `
          Fecha+Inicio%3a+${this.credito.fecha_inicio.replaceAll(" ", "+")}%0d%0a
-         Cliente%3a+${this.credito.cliente.alias}%0d%0a
+         Cliente%3a+${this.credito.cliente.alias.replaceAll(" ", "+")}%0d%0a
          Abonos%3a+$${this.credito.abonos}.00%0d%0a
          Atrasos%3a+${await this.calcularAtrasos()}%0d%0a
          Cuotas+pendientes%3a+${cuotasPendientesParse}+/+${this.credito.total_cuotas}.00%0d%0a%0d%0a
-         Informacion+ultimo+pago%3a+%0d%0a%0d%0a
+         Informacion+ultimo+pago%3a+%0d%0a
          Valor%3a+$${this.getLastPay().valor}.00%0d%0a
          Fecha%3a+${this.getLastPay().fecha.replaceAll(" ", "+")}
       `
