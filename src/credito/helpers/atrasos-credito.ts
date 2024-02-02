e7ad2931dc6b8b7b15ba602553e238a67f22ab7b
david cuspoca <david-cuspoca@hotmail.com>
2024-02-02 20:13:16 -0300

    atraso-credito.ts actualizado
    se refactorizo por completo este archivo, ademas de agregar la
    funcionalidad para enviar los mensajes de pago

--- src/credito/helpers/atrasos-credito.ts
+++ src/credito/helpers/atrasos-credito.ts
@@ -1,69 +1,91 @@
-import { addDays, differenceInDays, getDay } from "date-fns";
+import * as moment from 'moment';
 import { Credito } from "../entities/credito.entity";
+import { Pago } from 'src/pago/entities/pago.entity';
 
-export const calcularAtrasos = async (credito: Credito) => {
-   // const credito = await this.creditoModel.findById(idCredito);
-   let q = credito.fecha_inicio.split('/');
+export class CalculadorDeAtrasos {
 
-   let newFecha = `${q[2]}-${q[1]}-${q[0]}`;
+    private credito: Credito;
+    private fechaActual: moment.Moment;
 
+    constructor(credito: Credito) {
+        this.credito = credito;
+        this.fechaActual = moment();
+    }
 
-   const fechaInicioCredito = new Date(newFecha);
+    public getLastPay(): Pago {
 
-   let fechaInicioPagos = addDays(fechaInicioCredito, 1);
-
-   const fechaActual = new Date();
-
-   const totalDias = differenceInDays(fechaActual, fechaInicioPagos);
-
-   
-   const diasEfectivosPago = calcularDiasEfectivosPago(fechaInicioPagos, fechaActual, credito.se_cobran_domingos);
-   
-   const intervaloPagos = calcularIntervaloPagos(credito.frecuencia_cobro);
-
-   const valorCuota = credito.valor_cuota;
-   const totalAbonos = credito.abonos;
-
-   const pagosRealizados = Math.floor(totalAbonos / valorCuota);
-   const pagosRequeridos = credito.total_cuotas;
-
-   const atrasos = Math.max(diasEfectivosPago - pagosRealizados, 0);
-   const atrasosMaximos = calcularAtrasosMaximos(pagosRequeridos, intervaloPagos);
-
-   return Math.min(atrasos, atrasosMaximos);
- }
-
- export const calcularDiasEfectivosPago = (fechaInicioPagos: Date, fechaActual: Date, seCobranDomingos: boolean) => {
-
-   let diasEfectivosPago = differenceInDays(fechaActual, fechaInicioPagos);
-
-   if (!seCobranDomingos) {
-
-     let fecha = fechaInicioPagos;
-
-     for (let i = 0; i <= diasEfectivosPago; i++) {
-
-       if (getDay(fecha) === 0) {
-         diasEfectivosPago--;
-       }
-       fecha = addDays(fecha, 1);
-
-
-     }
+      return this.credito.pagos[0];
 
    }
- 
-   return diasEfectivosPago;
- }
 
- export const calcularIntervaloPagos = (frecuenciaCobro: string): number  => {
-   if (frecuenciaCobro === 'semanal') {
-     return 7;
-   } else {
-     return 1;
-   }
- }
+    private calcularDiasEfectivosPago(): number {
+        let diasEfectivosPago = this.fechaActual.diff(moment(this.credito.fecha_inicio, 'DD/MM/YYYY'), 'days');
+        
+        if (!this.credito.se_cobran_domingos) {
+          let fecha = moment(this.credito.fecha_inicio, 'DD/MM/YYYY');
+          for (let i = 0; i <= diasEfectivosPago; i++) {
+            if (fecha.day() === 0) {
+              diasEfectivosPago--;
+            }
+            fecha.add(1, 'day');
+          }
+        }
+        
+        return diasEfectivosPago;
+      }
 
- export const calcularAtrasosMaximos = (pagosRequeridos: number, intervaloPagos: number): number => {
-   return (pagosRequeridos - 1) * intervaloPagos;
- }
+    private calcularIntervaloPagos(): number {
+        return this.credito.frecuencia_cobro === 'semanal' ? 7 : 1;
+    }
+
+    private calcularAtrasosMaximos(): number {
+        return (this.credito.total_cuotas - 1) * this.calcularIntervaloPagos();
+    }
+
+    public calcularAtrasos(): number {
+        const diasEfectivosPago = this.calcularDiasEfectivosPago();
+        const pagosRealizados = Math.floor(this.credito.abonos / this.credito.valor_cuota);
+        const atrasos = diasEfectivosPago - pagosRealizados;
+        // const atrasosMaximos = this.calcularAtrasosMaximos();
+
+        // return Math.min(atrasos, atrasosMaximos);
+        return atrasos;
+    }
+
+    getMessage(): { message: string; urlMessage: string } {
+
+      let cuotasPendientes = this.credito.saldo / this.credito.valor_cuota;
+      let aplicandoFixed = cuotasPendientes.toFixed(2);
+
+      let cuotasPendientesParse = Number(aplicandoFixed);
+
+      let txtEncoded: string = `
+         Fecha+Inicio%3a+${this.credito.fecha_inicio.replaceAll(" ", "+")}%0d%0a
+         Cliente%3a+${this.credito.cliente.alias.replaceAll(" ", "+")}%0d%0a
+         Abonos%3a+$${this.credito.abonos}.00%0d%0a
+         Saldo%3a+$${this.credito.saldo}.00%0d%0a
+         Atrasos%3a+${this.calcularAtrasos()}%0d%0a
+         Cuotas+pendientes%3a+${cuotasPendientesParse}+/+${this.credito.total_cuotas}.00%0d%0a%0d%0a
+         Informacion+ultimo+pago%3a+%0d%0a
+         Valor%3a+$${this.getLastPay().valor}.00%0d%0a
+         Fecha%3a+${this.getLastPay().fecha.replaceAll(" ", "+")}
+      `
+
+      let txtMessage: string = `
+         Fecha: ${this.credito.fecha_inicio} 
+         Cliente: ${this.credito.cliente.alias} 
+         Abonos: $${this.credito.abonos}.00 
+         Saldo: $${this.credito.saldo}.00 
+         Atrasos: ${this.calcularAtrasos()} 
+         Cuotas Pendientes: ${cuotasPendientesParse} / ${this.credito.total_cuotas}  
+         Informacion Ultimo Pago 
+         Valor: $${this.getLastPay().valor}.00  
+         Fecha: ${this.getLastPay().fecha}
+      `
+
+      return {
+         urlMessage: `https://wa.me/${this.credito.cliente.telefono}?text=${txtEncoded}`,
+         message: txtMessage
+      }
+    }
+}
